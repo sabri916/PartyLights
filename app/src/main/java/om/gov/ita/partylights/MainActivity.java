975b1c09bcac2d3fe4628eec4d3a37ea3b7d158c
Sabri K <sbrkrs@gmail.com>
2017-01-17 23:14:01 +0400

    copied code

--- app/src/main/java/om/gov/ita/partylights/MainActivity.java
+++ app/src/main/java/om/gov/ita/partylights/MainActivity.java
@@ -3,8 +3,19 @@ package om.gov.ita.partylights;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.enums.PNStatusCategory;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         joinPartyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(MainActivity.this, PubnubListenerService.class);
                 startService(intent);
             }
--- app/src/main/java/om/gov/ita/partylights/PubnubListenerService.java
+++ app/src/main/java/om/gov/ita/partylights/PubnubListenerService.java
@@ -1,9 +1,15 @@
 package om.gov.ita.partylights;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.PowerManager;
 import android.util.Log;
+import android.widget.Toast;
+
 
 import com.pubnub.api.PNConfiguration;
 import com.pubnub.api.PubNub;
@@ -13,28 +19,72 @@ import com.pubnub.api.models.consumer.PNStatus;
 import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
 import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
 
+
 import java.util.Arrays;
 
 public class PubnubListenerService extends Service {
 
     private final static String TAG = "LightsService";
 
-    public PubnubListenerService() {
+
+    PNConfiguration pnConfiguration = new PNConfiguration();
+
+
+    String channel = "my_channel";
+    PowerManager.WakeLock wl = null;
+
+    private final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            String pnMsg = msg.obj.toString();
+
+            final Toast toast = Toast.makeText(getApplicationContext(), pnMsg, Toast.LENGTH_SHORT);
+            toast.show();
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    toast.cancel();
+                }
+            }, 200);
+
+        }
+    };
+
+    private void notifyUser(Object message) {
+
+        Message msg = handler.obtainMessage();
+
+        try {
+            final String obj = (String) message;
+            msg.obj = obj;
+            handler.sendMessage(msg);
+            Log.i("Received msg : ", obj.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
     public void onCreate() {
         super.onCreate();
 
-        Log.i(TAG,"On Create()");
-
-        PNConfiguration pnConfiguration = new PNConfiguration();
         pnConfiguration.setPublishKey(MainActivity.PUBLISH_KEY);
         pnConfiguration.setSubscribeKey(MainActivity.SUBSCRIBE_KEY);
         pnConfiguration.setSecure(false);
-
         PubNub pubnub = new PubNub(pnConfiguration);
+        pubnub.subscribe().channels(Arrays.asList("my_channel")).execute();
 
+        Toast.makeText(this, "PubnubService created...", Toast.LENGTH_LONG).show();
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "SubscribeAtBoot");
+        if (wl != null) {
+            wl.acquire();
+            Log.i("PUBNUB", "Partial Wake Lock : " + wl.isHeld());
+            Toast.makeText(this, "Partial Wake Lock : " + wl.isHeld(), Toast.LENGTH_LONG).show();
+        }
+
+        Log.i("PUBNUB", "PubnubService created...");
 
         pubnub.addListener(new SubscribeCallback() {
             @Override
@@ -57,23 +107,23 @@ public class PubnubListenerService extends Service {
             }
         });
 
-        pubnub.subscribe().channels(Arrays.asList("my_channel")).execute();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return Service.START_STICKY;
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.i(TAG,"On Destroy()");
+        if (wl != null) {
+            wl.release();
+            Log.i("PUBNUB", "Partial Wake Lock : " + wl.isHeld());
+            Toast.makeText(this, "Partial Wake Lock : " + wl.isHeld(), Toast.LENGTH_LONG).show();
+            wl = null;
+        }
+        Toast.makeText(this, "PubnubService destroyed...", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
