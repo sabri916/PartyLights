a45d03ce6e5e078df40d018473f6ed9645f97a48
Sabri K <sbrkrs@gmail.com>
2017-01-18 21:12:26 +0400

    I now have firebaseMessagingService

--- app/src/main/java/om/gov/ita/partylights/MainActivity.java
+++ app/src/main/java/om/gov/ita/partylights/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import static com.google.firebase.messaging.FirebaseMessaging.INSTANCE_ID_SCOPE;
+
 public class MainActivity extends AppCompatActivity {
 
     final static String PUBLISH_KEY = "pub-c-416f0b87-4dc6-4274-bd4a-59e529b6d044";
@@ -15,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //push token
 
         Button joinPartyButton = (Button) findViewById(R.id.btn_join_party);
         joinPartyButton.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/om/gov/ita/partylights/MyFirebaseMessagingService.java
@@ -0,0 +1,28 @@
+package om.gov.ita.partylights;
+
+import android.app.Service;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by sabri on 18/01/17.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.i(TAG,"OnMessageReceived");
+
+        Intent intent = new Intent(this, LightsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+
+    }
+}
