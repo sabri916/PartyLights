1ba2eb5b3a1ef9b333a9c1f95dd91a3245e992e9
Sabri K <sbrkrs@gmail.com>
2017-01-20 15:04:29 +0400

    to modify ui!

--- app/src/main/java/om/gov/ita/partylights/MainActivity.java
+++ app/src/main/java/om/gov/ita/partylights/MainActivity.java
@@ -1,9 +1,13 @@
 package om.gov.ita.partylights;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
@@ -32,9 +36,8 @@ import static com.google.firebase.messaging.FirebaseMessaging.INSTANCE_ID_SCOPE;
 
 public class MainActivity extends AppCompatActivity {
 
-    final static String PUBLISH_KEY = "pub-c-416f0b87-4dc6-4274-bd4a-59e529b6d044";
-    final static String SUBSCRIBE_KEY = "sub-c-8ee57362-dc91-11e6-a669-0619f8945a4f";
     private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,5 @@ public class MainActivity extends AppCompatActivity {
         //topic subscription
         FirebaseMessaging.getInstance().subscribeToTopic("lights");
         FirebaseMessaging.getInstance().subscribeToTopic("wakeup");
-
     }
 }
