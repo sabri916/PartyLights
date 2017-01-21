7cf401c96f9befb13e35a569562db8598604ccef
Sabri K <sbrkrs@gmail.com>
2017-01-21 19:07:07 +0400

    added video

--- app/src/main/java/om/gov/ita/partylights/MainActivity.java
+++ app/src/main/java/om/gov/ita/partylights/MainActivity.java
@@ -1,42 +1,36 @@
 package om.gov.ita.partylights;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.SurfaceTexture;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
+import android.view.Surface;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.Response.Listener;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.firebase.iid.FirebaseInstanceId;
+import android.view.TextureView;
+
+
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.RemoteMessage;
-import com.google.gson.JsonObject;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+
 
 import static com.google.firebase.messaging.FirebaseMessaging.INSTANCE_ID_SCOPE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
 
     private static final String TAG = "MainActivity";
+
+    private MediaPlayer mMediaPlayer;
+    private TextureView mTextureView;
+
+    private float mVideoWidth;
+    private float mVideoHeight;
 
 
     @Override
@@ -47,5 +41,131 @@ public class MainActivity extends AppCompatActivity {
         //topic subscription
         FirebaseMessaging.getInstance().subscribeToTopic("lights");
         FirebaseMessaging.getInstance().subscribeToTopic("wakeup");
+
+        mTextureView = (TextureView) findViewById(R.id.textureView);
+
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        calculateVideoSize();
+        updateTextureViewSize(width,height);
+        initView();
+    }
+
+    private void initView() {
+        // SurfaceTexture is available only after the TextureView
+        // is attached to a window and onAttachedToWindow() has been invoked.
+        // We need to use SurfaceTextureListener to be notified when the SurfaceTexture
+        // becomes available.
+        mTextureView.setSurfaceTextureListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mMediaPlayer != null) {
+            // Make sure we stop video and release resources when activity is destroyed.
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void calculateVideoSize() {
+        try {
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.video_background);
+            MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
+            metaRetriever.setDataSource(
+                    afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            String height = metaRetriever
+                    .extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
+            String width = metaRetriever
+                    .extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
+            mVideoHeight = Float.parseFloat(height);
+            mVideoWidth = Float.parseFloat(width);
+
+        } catch (NumberFormatException e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i2) {
+        Surface surface = new Surface(surfaceTexture);
+
+        try {
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.video_background);
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer
+                    .setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            mMediaPlayer.setSurface(surface);
+            mMediaPlayer.setLooping(true);
+
+            // don't forget to call MediaPlayer.prepareAsync() method when you use constructor for
+            // creating MediaPlayer
+            mMediaPlayer.prepareAsync();
+
+            // Play video when the media source is ready for playback.
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+            });
+
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (SecurityException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (IllegalStateException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    private void updateTextureViewSize(int viewWidth, int viewHeight) {
+        float scaleX = 1.0f;
+        float scaleY = 1.0f;
+
+        if (mVideoWidth > viewWidth && mVideoHeight > viewHeight) {
+            scaleX = mVideoWidth / viewWidth;
+            scaleY = mVideoHeight / viewHeight;
+        } else if (mVideoWidth < viewWidth && mVideoHeight < viewHeight) {
+            scaleY = viewWidth / mVideoWidth;
+            scaleX = viewHeight / mVideoHeight;
+        } else if (viewWidth > mVideoWidth) {
+            scaleY = (viewWidth / mVideoWidth) / (viewHeight / mVideoHeight);
+        } else if (viewHeight > mVideoHeight) {
+            scaleX = (viewHeight / mVideoHeight) / (viewWidth / mVideoWidth);
+        }
+
+        // Calculate pivot points, in our case crop from center
+        int pivotPointX = viewWidth / 2;
+        int pivotPointY = viewHeight / 2;
+
+        Matrix matrix = new Matrix();
+        matrix.setScale(scaleX, scaleY, pivotPointX, pivotPointY);
+
+        mTextureView.setTransform(matrix);
+        //mTextureView.setLayoutParams(new FrameLayout.LayoutParams(viewWidth, viewHeight));
+    }
+
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
     }
 }
