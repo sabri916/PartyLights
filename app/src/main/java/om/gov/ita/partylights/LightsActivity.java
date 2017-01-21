e251353100629d849c88a88690af0b9eb2e5d0a0
Sabri K <sbrkrs@gmail.com>
2017-01-22 00:44:58 +0400

    done!

--- app/src/main/java/om/gov/ita/partylights/LightsActivity.java
+++ app/src/main/java/om/gov/ita/partylights/LightsActivity.java
@@ -1,5 +1,6 @@
 package om.gov.ita.partylights;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
@@ -29,18 +30,104 @@ public class LightsActivity extends AppCompatActivity {
         relativeLayout.setBackgroundColor(Color.BLUE);
 
         Handler mHandler = new Handler();
+        final Random random = new Random(32);
 
-        for (int i=5000; i < 300000 ; i += 5000) {
+        //0 - 108 (5 seconds of random colours)
+        for (int i=0; i < 108000 ; i += 5000) {
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    int red = (int) (Math.random()*255);
-                    int green = (int) (Math.random()*255);
-                    int blue = (int) (Math.random()*255);
+                    int red = (int) (random.nextInt(255));
+                    int green = (int) (random.nextInt(255));
+                    int blue = (int) (random.nextInt(255));
                     relativeLayout.setBackgroundColor(Color.rgb(red,blue,green));
                 }
             },i);
         }
+
+        //108 - 112 (2 seconds of random colours)
+        for (int i=108000; i < 112000 ; i += 2000) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    int red = (int) (random.nextInt(255));
+                    int green = (int) (random.nextInt(255));
+                    int blue = (int) (random.nextInt(255));
+                    relativeLayout.setBackgroundColor(Color.rgb(red,blue,green));
+                }
+            },i);
+        }
+
+        //112 - 205 (5 seconds of random colours)
+        for (int i=112000; i < 205000 ; i += 5000) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    int red = (int) (random.nextInt(255));
+                    int green = (int) (random.nextInt(255));
+                    int blue = (int) (random.nextInt(255));
+                    relativeLayout.setBackgroundColor(Color.rgb(red,blue,green));
+                }
+            },i);
+        }
+
+        //205 - 222 (5 seconds of b&w colours)
+        for (int i=205000; i < 222000 ; i += 5000) {
+
+            if(i%10000 == 0) {
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        relativeLayout.setBackgroundColor(Color.BLACK);
+                    }
+                }, i);
+            }
+            else{
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        relativeLayout.setBackgroundColor(Color.WHITE);
+                    }
+                }, i);
+            }
+
+        }
+
+        //222 - 250 (5 seconds of random colours)
+        for (int i=222000; i < 250000 ; i += 5000) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    int red = (int) (random.nextInt(255));
+                    int green = (int) (random.nextInt(255));
+                    int blue = (int) (random.nextInt(255));
+                    relativeLayout.setBackgroundColor(Color.rgb(red,blue,green));
+                }
+            },i);
+        }
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LightsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        },26000);
+
+
+//        for (int i=5000; i < 300000 ; i += 5000) {
+//            mHandler.postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    int red = (int) (Math.random()*255);
+//                    int green = (int) (Math.random()*255);
+//                    int blue = (int) (Math.random()*255);
+//                    relativeLayout.setBackgroundColor(Color.rgb(red,blue,green));
+//                }
+//            },i);
+//        }
 
     }
 }
